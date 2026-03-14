0380773c7c9e38e7d1ace43ddda50a8326c8a01a
Brendon Thiede <brendon.thiede@gmail.com>
2026-03-14 18:33:36 -0400

    Refactor player movement logic to use velocity instead of direct position manipulation

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,21 +1,23 @@
 extends CharacterBody3D
 
 const SPEED: float = 7.0
-const JUMP_VELOCITY: float = 100.0
+const JUMP_VELOCITY: float = 10.0
 
 var sprint_speed_x: float = 0.0
 var sprint_speed_z: float = 0.0
 
 func _physics_process(delta: float) -> void:
-	
+	var horizontal_velocity_x: float = 0.0
+	var horizontal_velocity_z: float = 0.0
+
 	if Input.is_action_pressed("s"):
-		position.x -= SPEED * delta
+		horizontal_velocity_x -= SPEED
 	if Input.is_action_pressed("w"):
-		position.x += SPEED * delta
+		horizontal_velocity_x += SPEED
 	if Input.is_action_pressed("a"):
-		position.z -= SPEED * delta
+		horizontal_velocity_z -= SPEED
 	if Input.is_action_pressed("d"):
-		position.z += SPEED * delta
+		horizontal_velocity_z += SPEED
 		
 	if Input.is_action_pressed("shift"):
 		if Input.is_action_pressed("w"):
@@ -23,13 +25,13 @@ func _physics_process(delta: float) -> void:
 				sprint_speed_x += 12 * delta
 			sprint_speed_x += 10 * delta
 			sprint_speed_x = min(sprint_speed_x, 20.0)
-			position.x += sprint_speed_x * delta
+			horizontal_velocity_x += sprint_speed_x
 		elif Input.is_action_pressed("s"):
 			if sprint_speed_x > 0:
 				sprint_speed_x -= 12 * delta
 			sprint_speed_x -= 10 * delta
 			sprint_speed_x = max(sprint_speed_x, -20.0)
-			position.x += sprint_speed_x * delta
+			horizontal_velocity_x += sprint_speed_x
 		else:
 			if sprint_speed_x > 0:
 				sprint_speed_x -= 40 * delta
@@ -43,13 +45,13 @@ func _physics_process(delta: float) -> void:
 				sprint_speed_z += 12 * delta
 			sprint_speed_z += 10 * delta
 			sprint_speed_z = min(sprint_speed_z, 20.0)
-			position.z += sprint_speed_z * delta
+			horizontal_velocity_z += sprint_speed_z
 		elif Input.is_action_pressed("a"):
 			if sprint_speed_z > 0:
 				sprint_speed_z -= 12 * delta
 			sprint_speed_z -= 10 * delta
 			sprint_speed_z = max(sprint_speed_z, -20.0)
-			position.z += sprint_speed_z * delta
+			horizontal_velocity_z += sprint_speed_z
 		else:
 			if sprint_speed_z > 0:
 				sprint_speed_z -= 40 * delta
@@ -57,10 +59,12 @@ func _physics_process(delta: float) -> void:
 			if sprint_speed_z < 0:
 				sprint_speed_z += 40 * delta
 				sprint_speed_z = min(sprint_speed_z, 0.0)
+
+	velocity.x = horizontal_velocity_x
+	velocity.z = horizontal_velocity_z
 				
 	if not is_on_floor():
 		velocity += get_gravity() * 4 * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		velocity.y = max(position.y, 10.0)
 	move_and_slide()
